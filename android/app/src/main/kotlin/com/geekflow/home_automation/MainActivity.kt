cb5246c2f5c2a31cc73e65c0bf82c56b7ec557bc
Jobin Biju <jobinbiju9090@gmail.com>
2021-04-29 16:00:53 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/geekflow/home_automation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.geekflow.home_automation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(
+    MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    );
+  }
+}
