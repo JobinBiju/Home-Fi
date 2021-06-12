bc803628203215e052da4d2a53b598ebfd3dcba5
Jobin Biju <jobinbiju9090@gmail.com>
2021-06-12 11:42:49 +0530

    Changed initial route to splash screen

--- lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
+++ lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
@@ -5,14 +5,14 @@ import 'package:get/get.dart';
 
 class SplashScreenController extends GetxController
     with SingleGetTickerProviderMixin {
-// animation controller for lottie
+  // animation controller for lottie
   late AnimationController animationController;
 
   @override
   void onInit() {
     super.onInit();
     // spalsh animation config
-    AnimationController animationController = AnimationController(
+    animationController = AnimationController(
       duration: Duration(seconds: 4),
       vsync: this,
     );
