8d401d5a59ec2364822a4e7e4ff09e983b169b5b
Jobin Biju <jobinbiju9090@gmail.com>
2021-06-14 22:58:55 +0530

    Added a basic scanResultTile

--- lib/app/global_widgets/scan_result_tile.dart
+++ lib/app/global_widgets/scan_result_tile.dart
@@ -3,12 +3,52 @@ import 'package:flutter_blue/flutter_blue.dart';
 
 class ScanResultTile extends StatelessWidget {
   final ScanResult result;
-  final void Function() onTap;
-  const ScanResultTile({Key? key, required this.result, required this.onTap})
-      : super(key: key);
+  final VoidCallback onTap;
+
+  const ScanResultTile({
+    Key? key,
+    required this.result,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ExpansionTile(
+      title: _buildTitle(context),
+      leading: Text(result.rssi.toString()),
+      trailing: RaisedButton(
+        child: Text('Connect'),
+        color: Colors.black,
+        textColor: Colors.white,
+        onPressed: (result.advertisementData.connectable) ? onTap : null,
+      ),
+      children: [],
+    );
+  }
+
+  Widget _buildTitle(BuildContext context) {
+    if (result.device.name.length > 0) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            result.device.name,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Text(
+            result.device.id.toString(),
+            style: Theme.of(context).textTheme.caption,
+          )
+        ],
+      );
+    } else {
+      return Text(result.device.id.toString());
+    }
+  }
+
+  String getNiceHexArray(List<int> bytes) {
+    return '[${bytes.map((i) => i.toRadixString(16).padLeft(2, '0')).join(', ')}]'
+        .toUpperCase();
   }
 }
