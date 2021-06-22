2185bdf540643c018d912c1fd2e82be44e3c0d17
Jobin Biju <jobinbiju9090@gmail.com>
2021-06-22 13:40:29 +0530

    Temperature api added

---
+++ lib/app/data/provider/TempHumidAPI.dart
@@ -0,0 +1,8 @@
+class TempHumidAPI {
+  static String username = 'blairripper';
+  static String aioKey = '';
+  static String tempFeed = 'temperature';
+  static String humidFeed = 'humidity';
+  static String mainURL = 'https://io.adafruit.com/api/v2/';
+}
+// 'https://io.adafruit.com/api/v2/$username/feeds/$tempFeed?x-aio-key=$aioKey'
