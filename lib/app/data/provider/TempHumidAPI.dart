a19816bc004b6cee1b0bf305bc2fcb4fa373c0a7
Jobin Biju <jobinbiju9090@gmail.com>
2021-06-22 17:48:06 +0530

    API added to .env

--- lib/app/data/provider/TempHumidAPI.dart
+++ lib/app/data/provider/TempHumidAPI.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:home_fi/app/data/models/humidity_model.dart';
 import 'package:home_fi/app/data/models/temperature_model.dart';
 import 'package:http/http.dart' as http;
 
 class TempHumidAPI {
   static String username = 'blairripper';
-  static String aioKey = 'aio_fMWU75tbiBzTSMqubUNjBdjSDbyZ';
+  static String? aioKey = dotenv.env['API_Key'].toString();
   static String tempFeed = 'temperature';
   static String humidFeed = 'humidity';
   static String mainURL = 'https://io.adafruit.com/api/v2/';
@@ -13,18 +14,22 @@ class TempHumidAPI {
   static Future getTempData() async {
     http.Response response = await http.get(
         Uri.parse(mainURL + '$username/feeds/$tempFeed'),
-        headers: <String, String>{'X-AIO-Key': aioKey});
+        headers: <String, String>{'X-AIO-Key': aioKey!});
     if (response.statusCode == 200) {
       return Temperature.fromRawJson(response.body);
+    } else {
+      print(json.decode(response.body)['error']);
     }
   }
 
   static Future getHumidData() async {
     http.Response response = await http.get(
         Uri.parse(mainURL + '$username/feeds/$humidFeed'),
-        headers: <String, String>{'X-AIO-Key': aioKey});
+        headers: <String, String>{'X-AIO-Key': aioKey!});
     if (response.statusCode == 200) {
       return Humidity.fromRawJson(response.body);
+    } else {
+      print(json.decode(response.body)['error']);
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 import 'app/theme/app_theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
