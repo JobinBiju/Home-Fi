68f34629c6691e9c79263a636edad8712e78daac
Jobin Biju <jobinbiju9090@gmail.com>
2021-06-29 07:59:14 +0530

    Removed some comments from ESP32 code

--- ESP32 Code/ESP32_AdafruitIO.ino
+++ ESP32 Code/ESP32_AdafruitIO.ino
@@ -1,21 +1,3 @@
-// Adafruit IO Temperature & Humidity Example
-// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-temperature-and-humidity
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Written by Todd Treece for Adafruit Industries
-// Copyright (c) 2016-2017 Adafruit Industries
-// Licensed under the MIT license.
-//
-// All text above must be included in any redistribution.
-
-/************************** Configuration ***********************************/
-
-// edit the config.h tab and enter your Adafruit IO credentials
-// and any additional configuration needed for WiFi, cellular,
-// or ethernet clients.
 #include "config.h"
 
 /************************ Example Starts Here *******************************/
@@ -30,9 +12,9 @@
 #define LED_2 12
 
 // you should change these to match PWM pins on other platforms.
-#define RED_PIN   18
+#define RED_PIN 18
 #define GREEN_PIN 19
-#define BLUE_PIN  21
+#define BLUE_PIN 21
 
 // create DHT22 instance
 DHT_Unified dht(DATA_PIN, DHT11);
@@ -49,7 +31,8 @@ AdafruitIO_Feed *color = io.feed("color");
 
 int x = 0;
 
-void setup() {
+void setup()
+{
 
   // set LED pin as an output
   pinMode(LED_2, OUTPUT);
@@ -58,22 +41,23 @@ void setup() {
   Serial.begin(115200);
 
   // wait for serial monitor to open
-  while(! Serial);
+  while (!Serial)
+    ;
 
-  #if defined(ARDUINO_ARCH_ESP32) // ESP32 pinMode
-    // assign rgb pins to channels
-    ledcAttachPin(RED_PIN, 1);
-    ledcAttachPin(GREEN_PIN, 2);
-    ledcAttachPin(BLUE_PIN, 3);
-    // init. channels
-    ledcSetup(1, 12000, 8);
-    ledcSetup(2, 12000, 8);
-    ledcSetup(3, 12000, 8);
-  #else
-    pinMode(RED_PIN, OUTPUT);
-    pinMode(GREEN_PIN, OUTPUT);
-    pinMode(BLUE_PIN, OUTPUT);
-  #endif
+#if defined(ARDUINO_ARCH_ESP32) // ESP32 pinMode
+  // assign rgb pins to channels
+  ledcAttachPin(RED_PIN, 1);
+  ledcAttachPin(GREEN_PIN, 2);
+  ledcAttachPin(BLUE_PIN, 3);
+  // init. channels
+  ledcSetup(1, 12000, 8);
+  ledcSetup(2, 12000, 8);
+  ledcSetup(3, 12000, 8);
+#else
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+#endif
 
   // initialize dht22
   dht.begin();
@@ -86,7 +70,8 @@ void setup() {
   color->onMessage(handleMessageRGB);
 
   // wait for a connection
-  while(io.status() < AIO_CONNECTED) {
+  while (io.status() < AIO_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
@@ -99,57 +84,61 @@ void setup() {
   color->get();
 }
 
-void loop() {
+void loop()
+{
 
   // io.run(); is required for all sketches.
   // it should always be present at the top of your loop
   // function. it keeps the client connected to
   // io.adafruit.com, and processes any incoming data.
   io.run();
-  
+
   x++;
 
-  if(x==102){
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
+  if (x == 102)
+  {
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
 
-  float celsius = event.temperature;
-  float fahrenheit = (celsius * 1.8) + 32;
+    float celsius = event.temperature;
+    float fahrenheit = (celsius * 1.8) + 32;
 
-  Serial.print("celsius: ");
-  Serial.print(celsius);
-  Serial.println("C");
+    Serial.print("celsius: ");
+    Serial.print(celsius);
+    Serial.println("C");
 
-  Serial.print("fahrenheit: ");
-  Serial.print(fahrenheit);
-  Serial.println("F");
+    Serial.print("fahrenheit: ");
+    Serial.print(fahrenheit);
+    Serial.println("F");
 
-  // save fahrenheit (or celsius) to Adafruit IO
-  temperature->save(celsius);
+    // save fahrenheit (or celsius) to Adafruit IO
+    temperature->save(celsius);
 
-  dht.humidity().getEvent(&event);
+    dht.humidity().getEvent(&event);
 
-  Serial.print("humidity: ");
-  Serial.print(event.relative_humidity);
-  Serial.println("%");
-  Serial.println("................");
+    Serial.print("humidity: ");
+    Serial.print(event.relative_humidity);
+    Serial.println("%");
+    Serial.println("................");
 
-  // save humidity to Adafruit IO
-  humidity->save(event.relative_humidity);
-  x=0;
+    // save humidity to Adafruit IO
+    humidity->save(event.relative_humidity);
+    x = 0;
   }
 }
 
-void handleMessage(AdafruitIO_Data *data) {
+void handleMessage(AdafruitIO_Data *data)
+{
   Serial.print("received <- ");
 
-  if(data->toPinLevel() == HIGH)
+  if (data->toPinLevel() == HIGH)
     Serial.println("HIGH");
   else
     Serial.println("LOW");
   digitalWrite(LED_2, data->toPinLevel());
 }
-void handleMessageRGB(AdafruitIO_Data *data) {
+void handleMessageRGB(AdafruitIO_Data *data)
+{
 
   // print RGB values and hex value
   Serial.println("Received:");
@@ -162,14 +151,14 @@ void handleMessageRGB(AdafruitIO_Data *data) {
   Serial.print("  - HEX: ");
   Serial.println(data->value());
 
-  // invert RGB values for common anode LEDs
-  #if defined(ARDUINO_ARCH_ESP32) // ESP32 analogWrite
-    ledcWrite(1, data->toRed());
-    ledcWrite(2, data->toGreen());
-    ledcWrite(3, data->toBlue());
-  #else
-    analogWrite(RED_PIN, data->toRed());
-    analogWrite(GREEN_PIN, data->toGreen());
-    analogWrite(BLUE_PIN, data->toBlue());
-  #endif
+// invert RGB values for common anode LEDs
+#if defined(ARDUINO_ARCH_ESP32) // ESP32 analogWrite
+  ledcWrite(1, data->toRed());
+  ledcWrite(2, data->toGreen());
+  ledcWrite(3, data->toBlue());
+#else
+  analogWrite(RED_PIN, data->toRed());
+  analogWrite(GREEN_PIN, data->toGreen());
+  analogWrite(BLUE_PIN, data->toBlue());
+#endif
 }
